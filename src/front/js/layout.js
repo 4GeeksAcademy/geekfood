331b2166c8f833fc44366db24eca9b211aa5c876
David Nazariego Ulacio <dnazariego@gmail.com>
2024-07-24 19:22:49 +0000

    nazariego

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -14,6 +14,7 @@ import { Login } from "./pages/login";
 import { Registro } from "./pages/registro";
 import { NotFound } from "./pages/notfound";
 
+
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
