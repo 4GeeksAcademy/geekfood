34d54cf42c10174e903ebd49010ab2cfdbf21851
Franco Paraván <francoparavan@gmail.com>
2024-07-24 19:38:27 +0000

    franco

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,7 +10,6 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-import { Contact } from "./component/contact";
 
 
 //create your first component
@@ -32,7 +31,6 @@ const Layout = () => {
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Contact />
                     <Footer />
                 </ScrollToTop>
             </BrowserRouter>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,6 +4,7 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Navbar } from "../component/navbar";
 import { Jumbotron } from "../component/jumbotron";
+import { Contact } from "../component/contact";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -11,6 +12,7 @@ export const Home = () => {
 	return (
 		<>
 			<Jumbotron />
+			<Contact />
 		</>
 	);
 };
