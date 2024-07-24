1dc1a10b03f9beb4446af6fd4cd02585e4ac36db
Franco Paraván <francoparavan@gmail.com>
2024-07-24 16:11:25 +0000

    franco

---
+++ src/front/js/component/contact.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container text-center my-5">
+      <h2>¡Contáctanos!</h2>
+      <p>Estamos aquí para ayudarte.</p>
+      <div className="row">
+        <div className="col-md-4">
+          <i className="bi bi-envelope-fill" style={{ fontSize: '2rem' }}></i>
+          <h5 className="mt-3">Email</h5>
+          <p>Estamos aquí para ayudarte. Escríbenos al siguiente correo electronico</p>
+          <a href="mailto:hello@relume.io">hello@relume.io</a>
+        </div>
+        <div className="col-md-4">
+          <i className="bi bi-telephone-fill" style={{ fontSize: '2rem' }}></i>
+          <h5 className="mt-3">Teléfono</h5>
+          <p>Contactanos al siguiente numero telfonico</p>
+          <a href="tel:+1555000000">+1 (555) 000-0000</a>
+        </div>
+        <div className="col-md-4">
+          <i className="bi bi-geo-alt-fill" style={{ fontSize: '2rem' }}></i>
+          <h5 className="mt-3">Oficina</h5>
+          <p>También puedes visitarnos en nuestra oficina</p>
+          <address>123 Sample St, Sydney NSW 2000 AU</address>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
