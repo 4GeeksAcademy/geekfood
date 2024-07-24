ae784ac8f1398a2d6ed791222aed30fddee02a56
Franco Paraván <francoparavan@gmail.com>
2024-07-24 19:08:36 +0000

    franco

--- src/front/js/component/contact.js
+++ src/front/js/component/contact.js
@@ -9,20 +9,20 @@ export const Contact = () => {
         <div className="col-md-4">
           <i className="bi bi-envelope-fill" style={{ fontSize: '2rem' }}></i>
           <h5 className="mt-3">Email</h5>
-          <p>Estamos aquí para ayudarte. Escríbenos al siguiente correo electrónico</p>
+          <p>Escríbenos al siguiente correo electrónico.</p>
           <a href="mailto:hello@relume.io">hello@relume.io</a>
         </div>
         <div className="col-md-4">
           <i className="bi bi-telephone-fill" style={{ fontSize: '2rem' }}></i>
           <h5 className="mt-3">Teléfono</h5>
-          <p>Contáctanos al siguiente número telefónico</p>
-          <a href="tel:+1555000000">+1 (555) 000-0000</a>
+          <p>Contáctanos al siguiente número telefónico.</p>
+          <a href="">+1 (555) 000-0000</a>
         </div>
         <div className="col-md-4">
           <i className="bi bi-geo-alt-fill" style={{ fontSize: '2rem' }}></i>
           <h5 className="mt-3">Oficina</h5>
-          <p>También puedes visitarnos en nuestra oficina</p>
-          <address>123 Sample St, Sydney NSW 2000 AU</address>
+          <p>También puedes visitarnos en nuestra oficina.</p>
+          <p>123 Sample St, Sydney NSW 2000 AU</p>
         </div>
       </div>
     </div>
