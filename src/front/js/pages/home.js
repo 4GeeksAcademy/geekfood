f0695d24ef277570ad91f8bf3a1b488bd7137ebf
Danomolina <danomolina@gmail.com>
2024-07-24 19:08:23 +0000

    molina

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,6 +4,10 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Navbar } from "../component/navbar";
 import { Jumbotron } from "../component/jumbotron";
+import { Newsletter } from "../component/newsletter";
+import { DownloadApp } from "../component/DownloadApp";
+import { DiscoverProducts } from "../component/DiscoverProducts";
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -11,6 +15,10 @@ export const Home = () => {
 	return (
 		<>
 			<Jumbotron />
+			<DiscoverProducts />
+			<DownloadApp />
+			<Newsletter />
 		</>
+
 	);
 };
