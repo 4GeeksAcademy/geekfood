d6f4621303478de313f3ea95d4b5207e0a80ad07
Danomolina <danomolina@gmail.com>
2024-07-31 14:55:26 +0000

    danomolina

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,60 +2,65 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 import icon from "../../img/GEEK2.png";
+import { FaShoppingCart } from 'react-icons/fa';
 
+export const Navbar = ({ cart }) => {
+    const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-expand-lg">
-			<div className="container-fluid">
-				<a href="/"><img src={icon} alt="icon" className="navbar-icon" /></a>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link active" aria-current="page" to="/">Nosotros</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link active" aria-current="page" to="/">Servicios</Link>
-						</li>
-						<li className="nav-item dropdown">
-							<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-								Productos
-							</a>
-							<ul className="dropdown-menu">
-								<li><a className="dropdown-item" href="#">Americana</a></li>
-								<li><a className="dropdown-item" href="#">China</a></li>
-								<li><a className="dropdown-item" href="#">Coreana</a></li>
-								<li><a className="dropdown-item" href="#">Española</a></li>
-								<li><a className="dropdown-item" href="#">India</a></li>
-								<li><a className="dropdown-item" href="#">Italiana</a></li>
-								<li><a className="dropdown-item" href="#">Japonesa</a></li>
-								<li><a className="dropdown-item" href="#">Mexicana</a></li>
-							</ul>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link active" aria-current="page" to="/">Contactanos</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link active" aria-current="page" to="/faq">FAQ's</Link>
-						</li>
-						{/* <li className="nav-item">
-							<Link className="nav-link active" aria-current="page" to="/"><i className="bi bi-cart"></i>(0)</Link>
-						</li> */}
-					</ul>
-					<div className="d-flex me-2">
-						<Link className="btn btn-outline-secondary btn-sm" to="/registro">Registrate</Link>
-					</div>
-					<div className="d-flex">
-						<Link className="btn btn-outline-secondary btn-sm" to="/login">Inicia Sesión</Link>
-					</div>
-				</div>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <a href="/"><img src={icon} alt="icon" className="navbar-icon" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Nosotros</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Servicios</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Productos
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Americana</a></li>
+                                <li><a className="dropdown-item" href="#">China</a></li>
+                                <li><a className="dropdown-item" href="#">Coreana</a></li>
+                                <li><a className="dropdown-item" href="#">Española</a></li>
+                                <li><a className="dropdown-item" href="#">India</a></li>
+                                <li><a className="dropdown-item" href="#">Italiana</a></li>
+                                <li><a className="dropdown-item" href="#">Japonesa</a></li>
+                                <li><a className="dropdown-item" href="#">Mexicana</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Contactanos</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/faq">FAQ's</Link>
+                        </li>
+                    </ul>
+                    <div className="d-flex me-2">
+                        <Link className="btn btn-outline-secondary btn-sm" to="/registro">Registrate</Link>
+                    </div>
+                    <div className="d-flex me-2">
+                        <Link className="btn btn-outline-secondary btn-sm" to="/login">Inicia Sesión</Link>
+                    </div>
+                    <div className="navbar-cart d-flex">
+                        <Link to="/resumebuy" className="btn btn-outline-secondary btn-sm">
+                            <FaShoppingCart />
+                            {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
@@ -12,6 +12,9 @@ import ContactUs from "./pages/contactUs";
 import UserProfile from "./pages/userProfile";
 import injectContext from "./store/appContext";
 import DiscoverProducts from "./pages/discoverProducts";
+import ViewProducts from "./pages/viewproducts";
+import ProductDetail from "./pages/productDetail";
+import ResumeBuy from "./pages/resumebuy";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
@@ -24,12 +27,30 @@ import { Suscription } from "./pages/suscription";
 
 import { Contact } from "./component/contact";
 import { Product } from "./component/product";
-import { ProductDetail } from "./pages/productDetail";
 import { FPassword } from "./pages/fpassword";
 
 
 const Layout = () => {
     const basename = process.env.BASENAME || "";
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (dish) => {
+        const exists = cart.find(item => item.id === dish.id);
+        if (exists) {
+            setCart(cart.map(item => item.id === dish.id ? { ...item, quantity: item.quantity + dish.quantity } : item));
+        } else {
+            setCart([...cart, { ...dish, quantity: dish.quantity }]);
+        }
+    };
+
+    const removeFromCart = (id) => {
+        setCart(cart.filter(item => item.id !== id));
+    };
+
+    const updateCart = (id, delta) => {
+        setCart(cart.map(item => item.id === id ? { ...item, quantity: item.quantity + delta } : item));
+    };
+
 
     if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
@@ -37,7 +58,7 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+                    <Navbar cart={cart} />
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Login />} path="/login" />
@@ -45,16 +66,18 @@ const Layout = () => {
                         <Route element={<Registro />} path="/registro" />
                         <Route element={<Contact />} path="/contact" />
                         <Route element={<Product />} path="/product" />
-                        <Route element={<ProductDetail />} path="/productDetail" />
+                        <Route element={<ProductDetail addToCart={addToCart} />} path="/productDetail/:dishId" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<Faq />} path="/faq" />
                         <Route element={<Product_americano />} path="/product_americano" />
+                        <Route element={<ViewProducts />} path="/viewproducts/:restaurantId" />
                         <Route element={<ContactUs />} path="/contactUs" />
                         <Route element={<UserProfile />} path="/userProfile" />
                         <Route element={<NotFound />} path="*" />
                         <Route element={<Suscription />} path="/suscription" />
                         <Route element={<DiscoverProducts />} path="/discoverProduct" />
+                        <Route element={<ResumeBuy cart={cart} updateCart={updateCart} removeFromCart={removeFromCart} />} path="/resumebuy" />
 
                     </Routes>
                     <Footer />
